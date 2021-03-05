48b8590d463d0d0143a69b1cbfc1a2ed33bd611c
anish-jampana <anishrj2@illinois.edu>
2021-03-05 15:32:18 -0800

    Finished all helper methods necessary for evaluating the board (checking horizontal, vertical, diagonallefttoright, diagonalrighttoleft).

--- src/tictactoe.cc
+++ src/tictactoe.cc
@@ -70,21 +70,95 @@ BoardState Board::CheckHorizontal(vector<vector<char>> vector) {
   return isWinner;
 }
 
-//BoardState Board::CheckVertical(vector<vector<char>> vector) {
-//  int numOfXVertical = 0;
-//  int numOfOVertical = 0;
-//
-//  BoardState isWinner = BoardState::NoWinner;
-//
-//  for (int i = 0; i < boardSideLength; i++) {
-//    numOfXVertical = 0;
-//    numOfOVertical = 0;
-//    for (int j = 0; j < boardSideLength; j++) {
-//      if (vector.at(j).at(i) == 'x') {
-//        num
-//      }
-//    }
-//  }
-//}
+BoardState Board::CheckVertical(vector<vector<char>> vector) {
+  int numOfXVertical = 0;
+  int numOfOVertical = 0;
+
+  BoardState isWinner = BoardState::NoWinner;
+
+  for (int i = 0; i < boardSideLength; i++) {
+    numOfXVertical = 0;
+    numOfOVertical = 0;
+    for (int j = 0; j < boardSideLength; j++) {
+      if (vector.at(j).at(i) == 'x') {
+        numOfXVertical++;
+      } else if (vector.at(j).at(i) == 'o') {
+        numOfOVertical++;
+      }
+      if (numOfXVertical == boardSideLength) {
+        if (isWinner == BoardState::Owins || isWinner == BoardState::UnreachableState) {
+          isWinner = BoardState::UnreachableState;
+        } else {
+          isWinner = BoardState::Xwins;
+        }
+      } else if (numOfOVertical == boardSideLength) {
+        if (isWinner == BoardState::Xwins || isWinner == BoardState::UnreachableState) {
+          isWinner = BoardState::UnreachableState;
+        } else {
+          isWinner = BoardState::Owins;
+        }
+      }
+    }
+  }
+  return isWinner;
+}
+
+BoardState Board::CheckDiagonalLeftToRight(vector<vector<char>> vector) {
+  int numOfXDiagonal = 0;
+  int numOfODiagonal = 0;
+
+  BoardState isWinner = BoardState::NoWinner;
+
+  for (int i = 0, j = 0; i < boardSideLength; i++, j++) {
+    if (vector.at(i).at(j) == 'x') {
+      numOfXDiagonal++;
+    } else if (vector.at(i).at(j) == 'o') {
+      numOfODiagonal++;
+    }
+    if (numOfXDiagonal == boardSideLength) {
+      if (isWinner == BoardState::Owins || isWinner == BoardState::UnreachableState) {
+        isWinner = BoardState::UnreachableState;
+      } else {
+        isWinner = BoardState::Xwins;
+      }
+    } else if (numOfODiagonal == boardSideLength) {
+      if (isWinner == BoardState::Xwins || isWinner == BoardState::UnreachableState) {
+        isWinner = BoardState::UnreachableState;
+      } else {
+        isWinner = BoardState::Owins;
+      }
+    }
+  }
+  return isWinner;
+}
+
+BoardState Board::CheckDiagonalRightToLeft(vector<vector<char>> vector) {
+  int numOfXDiagonal = 0;
+  int numOfODiagonal = 0;
+
+  BoardState isWinner = BoardState::NoWinner;
+
+  for (int i = 0, j = boardSideLength - 1; i < boardSideLength; i++, j--) {
+    if (vector.at(i).at(j) == 'x') {
+      numOfXDiagonal++;
+    } else if (vector.at(i).at(j) == 'o') {
+      numOfODiagonal++;
+    }
+    if (numOfXDiagonal == boardSideLength) {
+      if (isWinner == BoardState::Owins || isWinner == BoardState::UnreachableState) {
+        isWinner = BoardState::UnreachableState;
+      } else {
+        isWinner = BoardState::Xwins;
+      }
+    } else if (numOfODiagonal == boardSideLength) {
+      if (isWinner == BoardState::Xwins || isWinner == BoardState::UnreachableState) {
+        isWinner = BoardState::UnreachableState;
+      } else {
+        isWinner = BoardState::Owins;
+      }
+    }
+  }
+  return isWinner;
+}
 
 }  // namespace tictactoe
